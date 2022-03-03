d573035b4e8b86ec89e031da5657021e52d244cd
CamDGG <camdeg98@kingshigh.school.nz>
2022-03-04 10:56:44 +1300

    update 4 march

---
+++ test.py
@@ -0,0 +1,110 @@
+# Task 1
+print("Hello World")
+print("Rocket League")
+print(5+5)
+print(4/5)
+print(10000*10000)
+print("Ms White")
+print("Kingshigh School")
+print("15")
+print("21915 hrS")
+print(2022+25)
+print(15+100)
+print(94)
+print(2022-1969)
+print("NFS")
+print("Maths")
+
+# Task 2
+print("top text\nbottom text")
+print("34+65")
+
+# Task 3
+print(5+10.0)
+print(10-5.0)
+
+# Task 4
+#print("hello world" + 24)
+#print("hello world" - 24)
+#print("hello world" / 5)
+
+
+# Task 5
+print("hello world" * 5)
+
+# Task 6
+str(55+65)
+int(55.0)
+float(44+10)
+#int("hello world")
+
+# Task 7
+# My song choice and my favourite lyric
+print("I Wanna be here for the rest of my life... Looking for sun rays, needin those good days Fly me away away, you're my holiday. By KSI Holiday")
+print("Oh Shit Its Brian")
+# A string is the hashtag in front of the sentence that makes it grey.
+# A whole number (not a fractional number) that can be positive, negative, or zero
+# which means it is a number that has a decimal place.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
