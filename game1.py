53bc62797de9f507d81a89f08e1f896b8d7977ab
CamDGG <camdeg98@kingshigh.school.nz>
2022-03-17 14:58:17 +1300

    hi

---
+++ game1.py
@@ -0,0 +1,44 @@
+from adventurelib import *
+
+
+
+
+@when("brush teeth")
+def brush_teeth():
+	print("You brush your teeth")
+@when("brush teeth")
+@when("brush")
+@when("clean teeth")
+def brush_teeth():
+	print("You brush your teeth")
+
+@when("comb hair")
+@when("comb")
+def comb_hair():
+	say("""
+		You brush your long flowing locks with the gold hairbrush that you have selected from the red basket
+		""")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+	start()
+
+if __name__ == '__main__':
+	main()
--- test.py
+++ test.py
@@ -42,24 +42,84 @@ float(44+10)
 # My song choice and my favourite lyric
 print("I Wanna be here for the rest of my life... Looking for sun rays, needin those good days Fly me away away, you're my holiday. By KSI Holiday")
 print("Oh Shit Its Brian")
-# A string is the hashtag in front of the sentence that makes it grey.
+# Strings are a collection of characters – these characters can be letters, symbols or even numbers. Each
+#of the following is a string in python
 # A whole number (not a fractional number) that can be positive, negative, or zero
 # which means it is a number that has a decimal place.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("183")
+print("Camron"*10)
+print(26.0+15.0) 
+print(5/7+4/8)
+#Question 9-18
+print(1000%9) #Modulo
+print(434//10) #Interger Division
+print(3%2)
+print(5%5)
+print(555/2)
+print(123/2)
+print(123//2)
+print(123%2)
+print(764%2)
+print(165//2)
+print(5/8)
+#26
+print("Camron"*10)									
+#Variables
+my_var = 1
+max_speed = 50
+acceleration = 10
+player_name = "Hero"
+gravity = 9.8
+#5.4 Excercises
+print(5)
+print("hero12345678")
+print(10000)
+print("022 347 987")
+user = ("camron")
+user = (15)
+user = (2022)
+user = print("camron")
+user = print(15)
+user = print(2022)
+user = ("de Groot-Green")
+user = ("Kingshigh")
+user = ("My Dads Home Pizza")
+user = ("Turtle")
+score = 0
+high_score = 0
+laps = 0
+ammuntion = 0
+racing_gear = 0
+speed_of_car = 0
+# 5.5 Modifying Variables
+money = 500
+money = 600
+#This make the 500 non exist on the computer and just read 600
+player_name = "Jack"
+player_name += " Smith"
+print(player_name)
+#5.6 Modifying Variables
+money = 100
+hat = 10
+top = 15
+pants = 20
+shoes = 25
+belt = 5
+print(money)
+print("buying a hat")
+print(money - hat)
+print("You now have the following amount left over")
+print("money = 100")
+#Buying Top
+print("buying top")
+print(money - top)
+print("You now have the following amount left over")
+print("money = 90")
+#Buying Pants
+print("buying pants")
+print(money - pants)
+print("You now have the following amount left over")
+print("money = 80")
 
 
 
