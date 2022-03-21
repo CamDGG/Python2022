8f703f2457f368160411ef4ad40f8061e33b0cb7
CamDGG <camdeg98@kingshigh.school.nz>
2022-03-22 09:56:32 +1300

    Today's Work adding more rooms

--- game1.py
+++ game1.py
@@ -1,6 +1,75 @@
 from adventurelib import *
 
 
+space = Room("""
+	You are drifting in space. It feels very cold.
+	A slate-blue spaceship sits completely silently to your left,
+	its airlock open and waiting.
+	""")
+
+spacecraft	= Room("""
+	The bridge of the spaceship is shiny and white, with thousands 
+	of small, red, blinking lights.
+	""")
+
+cargo = Room("""
+	The place where the ships come in and drop there cargo off
+	""")
+
+docking = Room("""
+	Where another spaceship is in space
+	""")
+
+
+hallway = Room("""
+	A very long hallway in the big Spacecraft
+	""")
+
+
+
+
+
+
+current_room = space
+
+@when("enter airlock")
+@when("enter spaceship")
+@when("enter ship")
+def enter_spaceship():
+	global current_room
+	#check if action can be done
+    if current_room is not space:
+    	say("There is no airlock here")
+        return
+
+	else:
+		current_room = spaceship 
+		print("""You heave yourself into the spaceship and
+		slam your hand on the button to close the door.
+		""")
+		print(current_room)
+	
+
+
+
+
+
+
+
+
+
+
+#Hallway = Room
+#Puzzle Room = Room 
+
+
+
+
+
+
+
+
+
 
 
 @when("brush teeth")
@@ -18,23 +87,6 @@ def comb_hair():
 	say("""
 		You brush your long flowing locks with the gold hairbrush that you have selected from the red basket
 		""")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def main():
